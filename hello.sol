cebe492b927245c728f7e15adf39a54b274bf983
madblocksgit <72651078+madblocksgit@users.noreply.github.com>
2021-03-26 16:33:07 +0530

    Create hello.sol

---
+++ hello.sol
@@ -0,0 +1,10 @@
+
+// Include the compiler version
+pragma solidity 0.7.0;
+
+// State Variables will be getting stored in the contract storage
+contract madhu {
+    string public a="Hello World"; // contract variable - state variable
+    uint public b=1234;
+}
+
